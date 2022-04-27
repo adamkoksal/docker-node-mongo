48a72b761d325654f27d01af9db7ecbad4bbcdfb
Adam Koksal <49299708+adamkoksal@users.noreply.github.com>
2022-04-26 22:34:06 -0500

    move build folder to client

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(
-  express.static(path.join(__dirname, "/build"))
+  express.static(path.join(__dirname, "../client/build"))
 );
 
 // Connect to MongoDB
